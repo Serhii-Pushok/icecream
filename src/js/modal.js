c381810e3b9a596f920d12600ebd64120ebc4d12
Vitalii Protsko <provitaleks@gmail.com>
2022-08-14 00:32:07 +0300

    connection js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,23 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalPurchaseBtn: document.querySelector("[data-modal-purchase-open]"),
+    openModalAboutBtn: document.querySelector("[data-modal-about-open]"),
+    closeModalPurchaseBtn: document.querySelector("[data-modal-purchase-close]"),
+    closeModalAboutBtn: document.querySelector("[data-modal-about-close]"),
+    modalPurchase: document.querySelector("[data-modal-purchase]"),
+    modalAbout: document.querySelector("[data-modal-about]"),
+  };
+
+  refs.openModalPurchaseBtn.addEventListener("click", toggleModalPurchase);
+  refs.openModalAboutBtn.addEventListener("click", toggleModalAbout);
+  refs.closeModalPurchaseBtn.addEventListener("click", toggleModalPurchase);
+  refs.closeModalAboutBtn.addEventListener("click", toggleModalAbout);
+
+  function toggleModalPurchase() {
+    refs.modalPurchase.classList.toggle("is-hidden");
+  }
+
+  function toggleModalAbout() {
+    refs.modalAbout.classList.toggle("is-hidden");
+  }
+})();
