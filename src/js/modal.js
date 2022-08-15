06b9ed053387e7ac412b938931ad7c5b24bd30bb
Vitalii Protsko <provitaleks@gmail.com>
2022-08-16 01:01:49 +0300

    add modal window franchise and fixit bags

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,18 +3,23 @@
     openModalPurchaseBtn: document.querySelector("[data-modal-purchase-open]"),
     openModalPurchaseMobilBtn: document.querySelector("[data-modal-purchase-mobil-open]"),
     openModalAboutBtn: document.querySelector("[data-modal-about-open]"),
+    openModalFranchiseBtn: document.querySelector("[data-modal-franchise-open]"),
     closeModalPurchaseBtn: document.querySelector("[data-modal-purchase-close]"),
     closeModalAboutBtn: document.querySelector("[data-modal-about-close]"),
+    closeModalFranchiseBtn: document.querySelector("[data-modal-franchise-close]"),
     modalPurchaseMobil: document.querySelector("[data-modal-purchase-mobil]"),
     modalPurchase: document.querySelector("[data-modal-purchase]"),
     modalAbout: document.querySelector("[data-modal-about]"),
+    modalFranchise: document.querySelector("[data-modal-franchise]"),
   };
 
   refs.openModalPurchaseBtn.addEventListener("click", toggleModalPurchase);
   refs.openModalPurchaseMobilBtn.addEventListener("click", toggleModalPurchaseMobil);
   refs.openModalAboutBtn.addEventListener("click", toggleModalAbout);
+  refs.openModalFranchiseBtn.addEventListener("click", toggleModalFranchise);
   refs.closeModalPurchaseBtn.addEventListener("click", toggleModalPurchase);
   refs.closeModalAboutBtn.addEventListener("click", toggleModalAbout);
+  refs.closeModalFranchiseBtn.addEventListener("click", toggleModalFranchise);
 
   function toggleModalPurchase() {
     refs.modalPurchase.classList.toggle("is-hidden");
@@ -26,5 +31,9 @@
 
   function toggleModalAbout() {
     refs.modalAbout.classList.toggle("is-hidden");
+  }
+
+  function toggleModalFranchise() {
+    refs.modalFranchise.classList.toggle("is-hidden");
   }
 })();
