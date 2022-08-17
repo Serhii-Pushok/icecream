1c66a725ceb5b7d67810aab028123415d59ded4e
Vitalii Protsko <provitaleks@gmail.com>
2022-08-17 16:52:43 +0300

    edit flight button

--- src/js/flying-button.js
+++ src/js/flying-button.js
@@ -2,17 +2,13 @@ function backToTop() {
     let button = $('.back-to-top');
 
     $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 50) {
+        if ($(this).scrollTop() >= 500) {
             button.fadeIn();
         } else {
             button.fadeOut();
         }
     });
 
-    bitton.on('click', (e) => {
-        e.preventDefaul();
-        $('html').animate({ scrollTop: 0 }, 1000);
-    })
 }
 
 backToTop();
